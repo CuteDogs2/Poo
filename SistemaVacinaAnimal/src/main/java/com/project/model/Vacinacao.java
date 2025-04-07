2a7d55b721e656a59433b11377117304acfcad69
automatedgothicat <solrachenrigit@gmail.com>
2025-04-07 10:11:58 -0400

    Small changes

--- SistemaVacinaAnimal/src/main/java/com/project/model/Vacinacao.java
+++ SistemaVacinaAnimal/src/main/java/com/project/model/Vacinacao.java
@@ -1,14 +1,31 @@
 import java.time.LocalDate;
 
 class Vacinacao {
-    private Animal animal ();
-    private Vacina vacina ();
-    private Veterinario veterinario ();
-    private LocalDate dataAplicacao ();
-    private String dosagem ();
-    private String lote ();
-    private String idFrasco();
+    private Animal animal;
+    private Vacina vacina;
+    private Veterinario veterinario;
+    private LocalDate dataAplicacao;
+    private String dosagem;
+    private String lote;
+    private String idFrasc;
 
+    Vacinacao(Animal animal, Vacina vacina, Veterinario veterinario, LocalDate dataAplicacao, String dosagem, String lote, String idFrasc){
+        this.animal = animal;
+        this.vacina = vacina;
+        this.veterinario = veterinario;
+        this.dataAplicacao = dataAplicacao;
+        this.dosagem = dosagem;
+        this.lote = lote;
+        this.idFrasc = idFrasc;
+    }
+
+    createVacinacao{
+        addVacina
+        addAnimal 
+    }
+    
+    public Vacina addVacina();
+    public Animal addAnimal();
     public void getAnimal();
     public void getVacina();
     public void getVeterinario();
