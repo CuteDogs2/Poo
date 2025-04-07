5b697476b3f054168846e0da3acbaa783a39300d
automatedgothicat <solrachenrigit@gmail.com>
2025-04-07 09:47:45 -0400

    Some changes

---
+++ SistemaVacinaAnimal/src/main/java/com/project/model/Vacinacao.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+
+class Vacinacao {
+    Animal animal ();
+    Vacina vacina ();
+    Veterinario veterinario ();
+    LocalDate dataAplicacao ();
+    String dosagem ();
+    String lote ();
+    String idFrasco();
+
+    1. getAnimal();
+    2. getVacina();
+    3. getVeterinario();
+    4. getDataAplicacao();
+    5. getDosagem();
+    6. getLote();
+    7. getidFrasco();
+}
