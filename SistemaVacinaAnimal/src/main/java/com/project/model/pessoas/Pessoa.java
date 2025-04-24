ece7b9127578381bf008ebb96380f8fff32cea16
João Felipe Marques Falcão <137521304+CuteDogs2@users.noreply.github.com>
2025-04-24 15:44:46 +0000

    pequenas correções na classe Pessoa

--- SistemaVacinaAnimal/src/main/java/com/project/model/pessoas/Pessoa.java
+++ SistemaVacinaAnimal/src/main/java/com/project/model/pessoas/Pessoa.java
@@ -1,6 +1,3 @@
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
 public class Pessoa {
     //atributos
     private String nome;
@@ -18,48 +15,8 @@ public class Pessoa {
         this.sexo = sexo;
         this.email = email;
     }
-    //METODOS ESPECIAIS
-    //verifico se o cpf tem o tamanho correto
-    public void verificarEmail(String email){
-        //estou passando o formato do email para a variável regex
-        String formato = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-        //compilando para que possa ser comparado posteiomente
-        Pattern pattern = Pattern.compile(formato);
-        //agora estou validadondo o email digitado
-        Matcher validar = pattern.matcher(email);
-        if(validar.matches()){
-            System.out.println("Email validado!");
-            setEmail(email);
-        }else{
-            System.out.println("Email inválido!!");
-        }
-    }
-    public void verificaCpf(String cpf) {
-        if (cpf.length() != 11) {
-            System.out.println("CPF inválido, tente novamente.");
-        } else {
-            setCpf(cpf);
-        }
-    }
-    //verifica se o número de telefone é valido
-    public void verificaTelefone(String telefone) {
-        if (telefone.length() != 11) {
-            System.out.println("Número de Telefone inválido, tente outro.");
-        } else {
-            setTelefone(telefone);
-        }
-    }
-    // verificando a validade do atrada do sexo
-    //compara as strings e o tamanho delas
-    public void verificarSexo(String sexo) {
-        if (!(sexo.toUpperCase().equals("MASCULINO") && sexo.length() == 9
-                || sexo.toUpperCase().equals("FEMININO") && sexo.length() == 8)) {
-            System.out.println("Sexo inválido!");
-        } else {
-            setSexo(sexo);
-        }
-    }
-    //funções gette e sette
+
+    //funções get e set
     public void setEmail(String email){
         this.email = email;
     }
@@ -92,6 +49,6 @@ public class Pessoa {
     }
 
     public String getCpf() {
-        return cpf;
+        return this.cpf;
     }
 }
