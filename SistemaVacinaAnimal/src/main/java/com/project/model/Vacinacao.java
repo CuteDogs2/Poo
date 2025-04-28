b17f539e46a2be0c06036d5d41375d5bd5159b2c
automatedgothicat <solrachenrigit@gmail.com>
2025-04-28 08:05:57 -0400

    dataRetorno

--- SistemaVacinaAnimal/src/main/java/com/project/model/Vacinacao.java
+++ SistemaVacinaAnimal/src/main/java/com/project/model/Vacinacao.java
@@ -5,15 +5,17 @@ class Vacinacao {
     private Vacina vacina;
     private Veterinario veterinario;
     private LocalDate dataAplicacao;
+    private LocalDate dataRetorno;
     private String dosagem;
     private String lote;
     private String idFrasc;
 
-    Vacinacao(Animal animal, Vacina vacina, Veterinario veterinario, LocalDate dataAplicacao, String dosagem, String lote, String idFrasc){
+    Vacinacao(Animal animal, Vacina vacina, Veterinario veterinario, LocalDate dataAplicacao, LocalDate dataRetorno, String dosagem, String lote, String idFrasc){
         this.animal = animal;
         this.vacina = vacina;
         this.veterinario = veterinario;
         this.dataAplicacao = dataAplicacao;
+        this.dataRetorno = dataRetorno;
         this.dosagem = dosagem;
         this.lote = lote;
         this.idFrasc = idFrasc;
@@ -31,6 +33,9 @@ class Vacinacao {
     public LocalDate setDataAplicacao(){
         this.dataAplicacao = dataAplicacao;
     }
+    public LocalDate setDataRetorno(){
+        this.dataRetorno = dataRetorno;
+    }
     public String setDosagem(){
         this.dosagem = dosagem;
     }
@@ -41,6 +46,14 @@ class Vacinacao {
         this.idFrasco = idFrasco;
     }
     
+    public LocalDate verificarDataRetorno(LocalDate dataRetorno){
+        if(dataRetorno != NULL){
+            return dataRetorno;
+        } else{System.out.println("Sem data de retorno. Dose unica.");
+            return NULL;
+        }
+    }
+
     public void showVacinacao(){
         System.out.println("Animal: "+this.animal+", recebeu a vacina "+this.vacina+"na data de "+this.dataAplicacao+". Id: "+this.idFrasco);
     }
