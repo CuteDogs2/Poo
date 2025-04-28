43c767eb68e164032f6925d530f47db2f5721d4c
automatedgothicat <solrachenrigit@gmail.com>
2025-04-28 07:52:16 -0400

    set functions

--- SistemaVacinaAnimal/src/main/java/com/project/model/Vacinacao.java
+++ SistemaVacinaAnimal/src/main/java/com/project/model/Vacinacao.java
@@ -19,15 +19,27 @@ class Vacinacao {
         this.idFrasc = idFrasc;
     }
    
-    public Vacina addVacina();
-    public Animal addAnimal();
-    public Animal getAnimal();
-    public Vacina getVacina();
-    public Veterinario getVeterinario();
-    public LocalDate getDataAplicacao();
-    public String getDosagem();
-    public String getLote();
-    public String getidFrasco();
+    public Animal setAnimal(){
+        this.animal = animal;
+    }
+    public Vacina setVacina(){
+        this.vacina = vacina;
+    }
+    public Veterinario setVeterinario(){
+        this.veterinario = veterinario;
+    }
+    public LocalDate setDataAplicacao(){
+        this.dataAplicacao = dataAplicacao;
+    }
+    public String setDosagem(){
+        this.dosagem = dosagem;
+    }
+    public String setLote(){
+        this.lote = lote;
+    }
+    public String setidFrasco(){
+        this.idFrasco = idFrasco;
+    }
     
     public void showVacinacao(){
         System.out.println("Animal: "+this.animal+", recebeu a vacina "+this.vacina+"na data de "+this.dataAplicacao+". Id: "+this.idFrasco);
