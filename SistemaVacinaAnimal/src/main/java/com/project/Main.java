a08c1d8dcb718d8dfe1b228c5e10be716e21f14e
João Felipe Marques Falcão <137521304+CuteDogs2@users.noreply.github.com>
2025-05-21 22:00:41 +0000

    iniciando desenvolvimento da interface grafica.

--- SistemaVacinaAnimal/src/main/java/com/project/Main.java
+++ SistemaVacinaAnimal/src/main/java/com/project/Main.java
@@ -1,7 +1,21 @@
 package com.project;
 
-public class Main{
-    public static void main(String args[]){
-        
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/view.fxml"));
+        primaryStage.setTitle("Sistema Vacina Animal");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
