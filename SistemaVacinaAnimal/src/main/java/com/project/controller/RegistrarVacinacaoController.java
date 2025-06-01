97d474c008d22cd023bde221473c40107ba69948
cutedogs2 <joao.falcao034@academico.ufgd.edu.br>
2025-06-01 19:14:54 -0400

    Interface gráfica 7%

--- SistemaVacinaAnimal/src/main/java/com/project/controller/RegistrarVacinacaoController.java
+++ SistemaVacinaAnimal/src/main/java/com/project/controller/RegistrarVacinacaoController.java
@@ -1,7 +1,46 @@
 package com.project.controller;
 
-//import javafx.fxml.FXML;
+
+
+
+import com.project.model.Vacina;
+import com.project.model.animais.Animal;
+import java.time.LocalDate;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 
 public class RegistrarVacinacaoController {
+
+    @FXML private TextField cpfCliente;
+    @FXML private ComboBox<Vacina> vacinaAplicada;
+    @FXML private DatePicker dataAplicacao;
+    @FXML private DatePicker dataRetorno;
+    @FXML private ComboBox<Animal> animalVacinado;
+    @FXML private ComboBox<String> identificacaoVeterinario;
+
+
+
+
+    public void initialize() {
+
+        //list<Vacina> vacinas = buscar todas as vacinass do banco de dados.
+        //
+
+    }
+
+
+
+
+    @FXML
+    private void onRegistrarVacinacaoClicked() {
+
+        String cpf = cpfCliente.getText();
+        //int vacina = vacinaAplicada.getValue();
+        LocalDate dateDeAplicacao = dataAplicacao.getValue();
+        LocalDate dateDeRetorno = dataRetorno.getValue();
+        
+
+    }
     
 }
