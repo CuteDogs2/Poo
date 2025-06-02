bc4b94f328d5e1c4c42e6637070af8be4ff79db2
cutedogs2 <joao.falcao034@academico.ufgd.edu.br>
2025-06-02 12:59:30 -0400

    Conexão com banco de dados

--- SistemaVacinaAnimal/src/main/java/com/project/Main.java
+++ SistemaVacinaAnimal/src/main/java/com/project/Main.java
@@ -22,6 +22,22 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+
+
+
+        // Testa a conexão com o banco de dados
+        try (java.sql.Connection conn = com.project.util.DataBaseUtil.getConnection()) {
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
+            }
+        } catch (Exception e) {
+            System.out.println("erro ao conectar: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+
+
+
         launch(args);
     }
 }
